c2c478ac392d9b546df5a5b44278c1edd9dce758
cocopi_dev <daikichiwanwan4649@gmail.com>
2020-08-14 17:01:56 +0900

    UPDATE: djangoのレコメンデーションシステムの実装

--- django/myapp/admin.py
+++ django/myapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Person_data #作成したモデルをimport
+from .models import Person_data, reco,  rank_id#作成したモデルをimport
 
 # Register your models here.
 admin.site.register(Person_data) #作成したモデルを追記
+
+admin.site.register(reco)
+admin.site.register(rank_id)
--- django/myapp/urls.py
+++ django/myapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PersonListApiView
 from .views import PersonDataViewSet
+from .views import RecoMovieApi
 
 router = DefaultRouter()
 router.register(r"persondata", PersonDataViewSet)
@@ -9,4 +10,5 @@ router.register(r"persondata", PersonDataViewSet)
 urlpatterns = [
     path('get_person/', PersonListApiView.as_view()),
     path("", include(router.urls)),
+    path('reco/', RecoMovieApi.as_view()),
 ]
